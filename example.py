bbd514dfd7ef40c33afe5601ba66c07d491ac679
bold <bold@cryptoguru.com>
2019-07-12 23:52:46 +0800

    port to python3 and numpy + small cleanups

--- example.py
+++ example.py
@@ -1,23 +1,28 @@
 import bwr
 import matplotlib.pyplot as plt
+import numpy as np
 
 # Read input csv file from physionet
-f = open('samples1.csv', 'r')
+f = open("samples/1.csv", "r")
 lines = f.readlines()
 f.close()
 
 # Discard the first two lines because of header. Takes either column 1 or 2 from each lines (different signal lead)
-signal = [0]*(len(lines)-2)
-for i in xrange(len(signal)):
-	signal[i] = float(lines[i+2].split(',')[1])
+signal = np.zeros((len(lines) - 2))
+for i in range(len(signal)):
+    signal[i] = float(lines[i + 2].split(",")[1])
 
-# Call the BWR method
-(baseline, ecg_out) = bwr.bwr(signal)
+baseline = bwr.calc_baseline(signal)
 
-plt.subplot(2,1,1)
-plt.plot(signal, 'b-')
-plt.plot(baseline, 'r-')
+# Remove baseline from orgianl signal
+ecg_out = signal - baseline
 
-plt.subplot(2,1,2)
-plt.plot(ecg_out, 'b-')
+plt.subplot(2, 1, 1)
+plt.plot(signal, "b-", label="signal")
+plt.plot(baseline, "r-", label="baseline")
+plt.legend()
+
+plt.subplot(2, 1, 2)
+plt.plot(ecg_out, "b-", label="signal - baseline")
+plt.legend()
 plt.show()
